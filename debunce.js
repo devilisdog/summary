3b17da764968a2bcd2f8fe60086192ecc721a355
陈超云 <378772004@qq.com>
2021-09-08 20:31:54 +0800

    防抖实现

---
+++ debunce.js
@@ -0,0 +1,50 @@
+//基础版
+function debunce_base(fun, wait) {
+    let timer
+
+    return function () {
+        clearTimeout(timer)
+        timer = setTimeout(fun, wait)
+    }
+}
+
+//改变this,event指向版
+function debunce_e(fun, wait) {
+    let timer
+    return function () {
+        let args = arguments //改变event指向
+
+        let _this = this
+        //console.log(this) // <div id='container'></div>  this指向当前对象的执行者
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fun.call(_this, ...args)
+        }, wait)
+    }
+}
+
+//立即执行版
+function debunce(fun, wait, immediate) {
+    let timer
+    return function () {
+        let args = arguments
+        let _this = this
+        clearTimeout(timer)
+        if (immediate) {
+            //wait时间内，持续滑动鼠标，callNow=false 不执行 fun.call(_this, ...args)
+            let callNow = !timer
+            timer = setTimeout(() => {
+                callNow = null
+            }, wait)
+
+            //立即执行
+            if (callNow) {
+                fun.call(_this, ...args)
+            }
+        } else {
+            timer = setTimeout(() => {
+                fun.call(_this, ...args)
+            }, wait)
+        }
+    }
+}
